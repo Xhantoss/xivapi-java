49426e610f1eaaaca31b54c0cc85f5708c083699
Lenard Jensen <lenardjensen@protonmail.com>
2020-06-24 14:42:46 +0200

    Character profile now completely implemented One can now receive data of characters with additional paramters for extra information.

--- src/test/java/xivapiTest.java
+++ src/test/java/xivapiTest.java
@@ -1,12 +1,14 @@
 import com.lenardjensen.xivapi;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class xivapiTest {
 
@@ -55,6 +57,34 @@ public class xivapiTest {
 			JSONObject charInfo = charJSON.getJSONObject("Character");
 			String name = charInfo.get("Name").toString();
 			assertEquals(name, "Adelbern Naxx");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	@Test
+	public void testGetCharByIDWithParams() {
+		// Lodestone ID of 'Adelbern Naxx'
+		int ID = 27075395;
+		try {
+			List<xivapi.dataParams> extraParams = Collections.singletonList(xivapi.dataParams.MIMO);
+			JSONObject charJSON = testedClass.getCharByID(ID, true, extraParams);
+			JSONObject charInfo = charJSON.getJSONObject("Character");
+			JSONArray minions = charJSON.getJSONArray("Minions");
+			String name = charInfo.get("Name").toString();
+			assertEquals(name, "Adelbern Naxx");
+
+			// Look for a specific minion
+			boolean hasRabbit = false;
+			for(int i = 0; i < minions.length(); i++) {
+				JSONObject minion = (JSONObject) minions.get(i);
+				if(minion.get("Name").toString().equals("Dwarf Rabbit")){
+					hasRabbit = true;
+					break;
+				}
+			}
+			assertTrue(hasRabbit);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
